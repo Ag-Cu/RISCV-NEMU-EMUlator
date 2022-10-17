e664320530c36167f30ddd9401df990c72c9cb6c
tracer-ics2022 <tracer@njuics.org>
2022-10-17 09:10:02 +0800

    >  compile NEMU SA22225455 尹童 Linux yt-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:10:02 up 15 min,  1 user,  load average: 1.91, 1.02, 0.64

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -14,14 +14,14 @@
 ***************************************************************************************/
 
 #include <isa.h>
-#include <memory/paddr.h>
-
+#include <me
 // this is not consistent with uint8_t
 // but it is ok since we do not access the array directly
 static const uint32_t img [] = {
   0x00000297,  // auipc t0,0
   0x0002b823,  // sd  zero,16(t0)
-  0x0102b503,  // ld  a0,16(t0)
+  0x0102b503,  // lmory/paddr.h>
+  d  a0,16(t0)
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
 };
