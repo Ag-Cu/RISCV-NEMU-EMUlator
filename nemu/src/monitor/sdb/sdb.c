9fa0c06b56eeb0973d88d6049eda2df568523f16
tracer-ics2022 <tracer@njuics.org>
2022-10-25 20:38:16 +0800

    >  compile NEMU SA22225455 尹童 Linux yt-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:38:16 up 10:19,  1 user,  load average: 0.01, 0.07, 0.14

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -82,11 +82,7 @@ static int cmd_x(char *args) {
     printf("No arguements given!\n");
     return -1;
   }
-  char *addr_str = strtok(NULL, " 0x");
-  if (addr_str == NULL) {
-    printf("No address arguement given!\n");
-    return -1;
-  }
+  char *addr_str = args + strlen(len) + 1;
   int mem_len = atoi(len);
   paddr_t addr = atol(addr_str);
   int temp;
