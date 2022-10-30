0ba7be1a3f7fb818b2b7b7b92e0cdf862a9cabe7
tracer-ics2022 <tracer@njuics.org>
2022-10-30 10:15:18 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.68.1-microsoft-standard-WSL2 #1 SMP Mon Sep 19 19:14:52 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:15:18 up 21 min,  0 users,  load average: 0.00, 0.02, 0.06

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -31,8 +31,27 @@ static char *code_format =
 "  return 0; "
 "}";
 
+void gen(char c) {
+  
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+  switch (choose(3))
+  {
+  case 0:
+    gen_num();
+    break;
+  case 1:
+    gen('(');
+    gen_rand_expr();
+    gen(')');
+    break;
+  default:
+    gen_rand_expr();
+    gen_rand_op();
+    gen_rand_expr();
+    break;
+  }
 }
 
 int main(int argc, char *argv[]) {
