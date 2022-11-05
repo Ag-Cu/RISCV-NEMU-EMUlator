0f2f3d969a489ef84dc3d8b037d45074e74a5c20
tracer-ics2022 <tracer@njuics.org>
2022-11-05 11:27:58 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.68.1-microsoft-standard-WSL2 #1 SMP Mon Sep 19 19:14:52 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:27:58 up  6:30,  0 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -41,7 +41,7 @@ static struct rule {
 
   {"0[xX][0-9]+", TK_HEX},            // hexadecimal-number
   {"^[1-9][0-9]*|0", TK_NUM},         // decimal nums (neg & pos & zero)
-  {"\\$[a-z]+", TK_REG},              // reg_name
+  {"\\$[a-z|0-9]+", TK_REG},              // reg_name
   {" +", TK_NOTYPE},                  // spaces
   {"\\+", TK_PLUS},                   // plus
   {"==", TK_EQ},                      // equal
