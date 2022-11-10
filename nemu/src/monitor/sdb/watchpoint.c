5fdc255a9cdfc8ad6ee0a98706e8d389657681db
tracer-ics2022 <tracer@njuics.org>
2022-11-10 22:25:56 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.68.1-microsoft-standard-WSL2 #1 SMP Mon Sep 19 19:14:52 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:25:56 up  6:25,  0 users,  load average: 0.09, 0.04, 0.01

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -28,8 +28,6 @@ void init_wp_pool() {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
-  wp_head->next = NULL;
-  wp_tail = wp_head;
   free_ = wp_pool;
   free_tail = wp_pool + NR_WP;
 }
