41e34849d35e6bc2ddefa53a6517e102232e35e3
tracer-ics2022 <tracer@njuics.org>
2022-12-26 11:54:13 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:54:13 up 14:52,  0 users,  load average: 0.14, 0.05, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,91 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+
+  while(*fmt != '\0') {
+    if(*fmt == '%') { // 如果是%开头，说明是格式化输出
+      fmt++;
+      switch(*fmt) {  // 根据后面的字符，输出不同的格式
+        case 'd': {
+          int num = va_arg(ap, int);  // 从ap中取出一个int类型的参数
+          char buf[20]; // 用于存放转换后的字符串
+          int i = 0;
+          if(num < 0) { // 如果是负数，先输出一个负号
+            out[i++] = '-';
+            num = -num;
+          }
+          do {  // 将数字转换成字符串
+            buf[i++] = num % 10 + '0';
+            num /= 10;
+          } while(num != 0);
+          while(i > 0) {  
+            *out++ = buf[--i];
+          }
+          break;
+        }
+        case 's': {
+          char *str = va_arg(ap, char *); 
+          while(*str != '\0') {
+            *out++ = *str++;
+          }
+          break;
+        }
+        case 'x': {
+          int num = va_arg(ap, int);
+          char buf[20];
+          int i = 0;
+          do {
+            int tmp = num % 16;
+            if(tmp < 10) {
+              buf[i++] = tmp + '0';
+            } else {
+              buf[i++] = tmp - 10 + 'a';
+            }
+            num /= 16;
+          } while(num != 0);
+          while(i > 0) {
+            *out++ = buf[--i];
+          }
+          break;
+        }
+        case 'p': {
+          int num = va_arg(ap, int);
+          char buf[20];
+          int i = 0;
+          do {
+            int tmp = num % 16;
+            if(tmp < 10) {
+              buf[i++] = tmp + '0';
+            } else {
+              buf[i++] = tmp - 10 + 'a';
+            }
+            num /= 16;
+          } while(num != 0);
+          while(i > 0) {
+            *out++ = buf[--i];
+          }
+          break;
+        }
+        case 'c': {
+          char c = va_arg(ap, int);
+          *out++ = c;
+          break;
+        }
+        default: {
+          *out++ = *fmt;
+          break;
+        }
+      }
+    } else {
+      *out++ = *fmt;
+    }
+    fmt++;
+  }
+    
+  va_end(ap);
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
