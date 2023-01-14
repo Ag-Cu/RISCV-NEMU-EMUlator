01db3b4cd28e2fe9b6c3ae19d9f617a6f2bded73
tracer-ics2022 <tracer@njuics.org>
2023-01-14 20:13:04 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:13:04 up 16:40,  0 users,  load average: 0.01, 0.02, 0.00

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -68,6 +68,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE_COND
+  printf("Memory write --> addr: 0x%08x   len: %d   data: 0x%lx\n", addr, len, data);
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
