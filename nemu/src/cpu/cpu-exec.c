7bb4e56b8f4ebb830d99a031df016cc9c6febb60
tracer-ics2022 <tracer@njuics.org>
2023-01-16 22:18:20 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:18:20 up 1 day,  4:01,  0 users,  load average: 0.11, 0.06, 0.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -67,7 +67,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 #ifdef CONFIG_WATCHPOINT
-
+    if(!check_wp()) {
+      nemu_state.state = NEMU_STOP;
+    }
 #endif
 }
 
