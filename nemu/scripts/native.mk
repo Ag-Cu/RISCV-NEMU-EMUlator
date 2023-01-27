71aebb6c6313713b6c18c2b38bcd39337c2f9c24
tracer-ics2022 <tracer@njuics.org>
2023-01-27 22:42:23 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:42:23 up 10:51,  0 users,  load average: 0.07, 0.02, 0.00

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -28,6 +28,7 @@ $(BINARY): compile_git
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt		
 override ARGS += $(ARGS_DIFF)
 override ARGS += $(ARGS_BATCH)
+override ARGS += $(ARGS_FTRACE)
 
 
 # Command to execute NEMU
--- nemu/tools/ftrace.mk
+++ nemu/tools/ftrace.mk
@@ -1,3 +1,3 @@
 ifdef CONFIG_FTRACE_COND
-ARGS_FTRACE = --elf=$(AM_HOME)/tests/cpu-tests/build
+ARGS_FTRACE = --elf=$(AM_HOME)/tests/cpu-tests/build/if-else-riscv64-nemu.elf
 endif
