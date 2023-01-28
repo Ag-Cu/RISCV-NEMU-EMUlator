c9bed0f5ec2359dd5f6f57f3522db02ab9fe4b19
tracer-ics2022 <tracer@njuics.org>
2023-01-28 18:11:55 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:11:55 up 21:10,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/tools/ftrace.mk
+++ nemu/tools/ftrace.mk
@@ -1,3 +1,3 @@
 ifdef CONFIG_FTRACE_COND
-ARGS_FTRACE = --elf=/home/yt/ics2022/am-kernels/tests/cpu-tests/build/recursion-riscv64-nemu.elf
+ARGS_FTRACE = --elf=/home/yt/ics2022/am-kernels/tests/cpu-tests/build/if-else-riscv64-nemu.elf
 endif
