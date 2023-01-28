3773a4bab5ec277b8c22964a16823aaa883dcd59
tracer-ics2022 <tracer@njuics.org>
2023-01-28 10:10:48 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:10:48 up 13:08,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/ftrace/elf_loader.c
+++ nemu/src/monitor/ftrace/elf_loader.c
@@ -21,11 +21,11 @@ void init_elf(const char *elf_file) {
     // TODO();
     // Log("Initializing elf tables...");
     FILE *fp = read_file(elf_file);
-    Elf32_Ehdr ehdr;                // elf header
+    Elf64_Ehdr ehdr;                // elf header
     int ret1 = fread(&ehdr, sizeof(ehdr), 1, fp);
     assert(ret1 == 1);
 
-    Elf32_Shdr shdr;                // section header
+    Elf64_Shdr shdr;                // section header
     fseek(fp, ehdr.e_shoff, SEEK_SET);
     
     // read section header
@@ -33,17 +33,17 @@ void init_elf(const char *elf_file) {
         int ret2 = fread(&shdr, sizeof(shdr), 1, fp);
         assert(ret2 == 1);
         // 打印section header
-        printf("sh_name: %d, sh_type: %d, sh_flags: %d, sh_addr: %d, sh_offset: %d, sh_size: %d, sh_link: %d, sh_info: %d, sh_addralign: %d, sh_entsize: %d\n",
+        printf("sh_name: %d, sh_type: %d, sh_flags: %ld, sh_addr: %ld, sh_offset: %ld, sh_size: %ld, sh_link: %d, sh_info: %d, sh_addralign: %ld, sh_entsize: %ld\n",
          shdr.sh_name, shdr.sh_type, shdr.sh_flags, shdr.sh_addr, shdr.sh_offset, shdr.sh_size, shdr.sh_link, shdr.sh_info, shdr.sh_addralign, shdr.sh_entsize);
         if (shdr.sh_type == SHT_SYMTAB) {
             // read symbol table
-            Elf32_Sym sym;
+            Elf64_Sym sym;
             fseek(fp, shdr.sh_offset, SEEK_SET);        // move to symbol table
             int i = 0;
             for (int j = 0; j < shdr.sh_size / sizeof(sym); j++) {      // read symbol table
                 int ret3 = fread(&sym, sizeof(sym), 1, fp);
                 assert(ret3 == 1);
-                if (ELF32_ST_TYPE(sym.st_info) == STT_FUNC) {  // ELF32_ST_TYPE的作用是取出符号类型
+                if (ELF64_ST_TYPE(sym.st_info) == STT_FUNC) {  // ELF64_ST_TYPE的作用是取出符号类型
                     // read function name
                     char *func_name = (char *)malloc(32);
                     fseek(fp, shdr.sh_link * sizeof(shdr) + ehdr.e_shoff + sym.st_name, SEEK_SET);      // move to function name
