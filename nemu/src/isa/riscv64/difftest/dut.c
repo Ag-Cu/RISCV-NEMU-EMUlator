05d686a11e7d405282d584c67e328c85ab9e69fd
tracer-ics2022 <tracer@njuics.org>
2023-02-05 21:52:02 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:52:02 up  8:46,  0 users,  load average: 0.04, 0.08, 0.14

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -18,12 +18,8 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  for (int i = 0; i < 32; i ++) {
-    if (ref_r->gpr[i] != gpr(i)) {
-      return false;
-    }
-  }
-  return true;
+  
+  return false;
 }
 
 void isa_difftest_attach() {
