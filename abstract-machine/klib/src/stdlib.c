14875e98a678e0b79444c07c916763bae2230101
tracer-ics2022 <tracer@njuics.org>
2023-02-07 22:01:56 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:01:56 up 13:51,  0 users,  load average: 0.05, 0.08, 0.03

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -41,7 +41,6 @@ void *malloc(size_t size) {
   size  = (size_t)ROUNDUP(size, 8);
   char *old = hbrk;
   hbrk += size;
-  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
   for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
     *p = 0;
   }
