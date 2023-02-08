248e28e20cd37c101d63ff00b46b59b7cdd0e35c
tracer-ics2022 <tracer@njuics.org>
2023-02-08 11:28:35 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:28:35 up 19:23,  0 users,  load average: 0.09, 0.08, 0.02

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,11 +1,11 @@
 #include <am.h>
 #include <nemu.h>
-
 #include "../../../riscv/riscv.h"
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = inb(KBD_ADDR + 4);
-  kbd->keycode = inl(KBD_ADDR);
+  uint32_t key = inl(KBD_ADDR);
+  kbd->keydown = key & KEYDOWN_MASK;
+  kbd->keycode = key & ~KEYDOWN_MASK;
 }
