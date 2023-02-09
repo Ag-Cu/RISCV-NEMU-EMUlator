649b7d419618fe9cc012bf025052a61a2c4180d4
tracer-ics2022 <tracer@njuics.org>
2023-02-09 14:45:27 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:45:27 up 1 day, 13:42,  0 users,  load average: 0.20, 0.15, 0.10

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -14,7 +14,6 @@ void __am_gpu_init() {
   // int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
   // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   // for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
