9a696528a4ad230f2be6cc06ee17dc46f18ab72e
tracer-ics2022 <tracer@njuics.org>
2023-02-25 11:52:25 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:52:25 up  1:13,  0 users,  load average: 0.08, 0.03, 0.01

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -31,6 +31,7 @@
 #define Divw div_divw
 #define Ecall ecall_helper
 #define Csrrs isa_csrrs
+#define Csrrw isa_csrrw
 
 word_t register_addi(word_t imm, int idx);
 word_t jump_jal(int64_t imm, Decode *s, int dest);
@@ -39,6 +40,7 @@ word_t cmp_and_return(uint64_t src1, uint64_t imm, int type);
 void branch(uint64_t src1, uint64_t src2, uint64_t imm, Decode *s, int type);
 word_t div_divw(word_t src1, uint64_t src2);
 word_t isa_csrrs(word_t dest, word_t src1, word_t csr);
+word_t isa_csrrw(word_t dest, word_t src1, word_t csr);
 void ecall_helper(Decode *s);
 
 extern word_t isa_raise_intr(word_t NO, vaddr_t epc);
@@ -150,6 +152,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 111 ????? 01110 11", remuw  , R, Reg(dest) = SEXT(BITS(src1, 31, 0) % BITS(src2, 31, 0), 32));
   INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem    , R, Reg(dest) = (int64_t)src1 % (int64_t)src2);
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, Csrrs(dest, src1, imm));
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, Csrrw(dest, src1, imm));
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, Ecall(s));
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , R, s->dnpc = CSRs.mepc);
 
@@ -271,6 +274,7 @@ word_t div_divw(word_t src1, word_t src2) {
   return SEXT(divisor / dividend, 32);
 }
 
+// 30571073                  0011 0000 0101 0111 0001 0000 0111 0011
 word_t isa_csrrs(word_t dest, word_t src1, word_t csr) {
   word_t t = 0;
   switch (csr) {
@@ -289,6 +293,32 @@ word_t isa_csrrs(word_t dest, word_t src1, word_t csr) {
     case 0x305:   // mtvec
       t = CSRs.mtvec;
       CSRs.mtvec = src1 | t;
+      break;
+    default:
+      panic("Unkown csr!");
+      break;
+  }
+  return t;
+}
+
+word_t isa_csrrw(word_t dest, word_t src1, word_t csr) {
+  word_t t = 0;
+  switch (csr) {
+    case 0x341:   // mepc
+      t = CSRs.mepc;
+      CSRs.mepc = src1;
+      break;
+    case 0x342:   // mcause
+      t = CSRs.mcause;
+      CSRs.mcause = src1;
+      break;
+    case 0x300:   // mstatus
+      t = CSRs.mstatus;
+      CSRs.mstatus = src1;
+      break;
+    case 0x305:   // mtvec
+      t = CSRs.mtvec;
+      CSRs.mtvec = src1;
       break;
     default:
       panic("Unkown csr!");
