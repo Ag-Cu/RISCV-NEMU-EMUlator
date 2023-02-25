b44e09c7bdc454d0b5d8d04c7fc9e1d8bca7b5e8
tracer-ics2022 <tracer@njuics.org>
2023-02-25 13:06:38 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:06:38 up  2:27,  0 users,  load average: 0.07, 0.05, 0.00

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -21,12 +21,12 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  CSRs.mstatus = 1;
+  CSRs.mstatus = 0;
   CSRs.mepc = epc;
   CSRs.mcause = NO;
   vaddr_t mtvec = CSRs.mtvec;
   return mtvec;
-} 
+}
 
 word_t isa_query_intr() {
   return INTR_EMPTY;
