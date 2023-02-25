de180854cf3f2d1953cfbebf8fabacad56177ad6
tracer-ics2022 <tracer@njuics.org>
2023-02-25 19:25:06 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:06 up  8:15,  0 users,  load average: 0.00, 0.03, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -18,6 +18,7 @@ Context* __am_irq_handle(Context *c) {
 
     c = user_handler(ev, c);
     assert(c != NULL);
+    c->mepc += 4;     // skip the instruction that caused the trap
   }
 
   return c;
