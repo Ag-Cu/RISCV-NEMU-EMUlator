a3b3895c9acdf3e75abb9ab569b0e895f8594a9d
tracer-ics2022 <tracer@njuics.org>
2023-02-26 14:50:25 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:50:25 up  1:04,  0 users,  load average: 0.01, 0.02, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,30 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+// 从ramdisk中`offset`偏移处的`len`字节读入到`buf`中
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+// 把`buf`中的`len`字节写入到ramdisk中`offset`偏移处
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+// 返回ramdisk的大小, 单位为字节
+size_t get_ramdisk_size();
+
+
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  Elf_Ehdr elf;
+  ramdisk_read(&elf, 0, sizeof(elf));
+  
+  Elf_Phdr ph;
+  for (int i = 0; i < elf.e_phnum; i++) {
+    ramdisk_read(&ph, elf.e_phoff + i * sizeof(ph), sizeof(ph));
+    if (ph.p_type == PT_LOAD) {
+      ramdisk_read((void *)ph.p_vaddr, ph.p_offset, ph.p_filesz);
+      memset((void *)(ph.p_vaddr + ph.p_filesz), 0, ph.p_memsz - ph.p_filesz);
+    }
+  }
+  
   return 0;
 }
 
