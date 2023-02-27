acee1ed1764c98c581d815d6ea934d2420b7c4a4
tracer-ics2022 <tracer@njuics.org>
2023-02-27 22:55:05 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:55:05 up 11:02,  0 users,  load average: 0.00, 0.01, 0.00

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -8,9 +8,9 @@ struct Context {
   void *pdir;
 };
 
-#define GPR1 gpr[17] // a7
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+#define GPR1 gpr[17]  // a7
+#define GPR2 gpr[0]   // a0
+#define GPR3 gpr[0]   // a1
+#define GPR4 gpr[0]   // a2
+#define GPRx gpr[0]   // a0
 #endif
