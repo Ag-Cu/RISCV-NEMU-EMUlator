de0e6395612c3becbc4a9cc172116778363ccf07
tracer-ics2022 <tracer@njuics.org>
2023-02-27 15:41:27 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:41:27 up  3:48,  0 users,  load average: 0.14, 0.09, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,7 +1,12 @@
 #include <am.h>
 #include <nemu.h>
-#include<klib.h>
+
+#include "../../../riscv/riscv.h"
+
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+
+# define W    400
+# define H    300
 
 void __am_gpu_init() {
   int i;
@@ -13,34 +18,31 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t info = inl(VGACTL_ADDR);
+  uint16_t height = (uint16_t)(info & 0xFFFF);
+  uint16_t width = (uint16_t)(info >> 16);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = inw(VGACTL_ADDR+2), .height = inw(VGACTL_ADDR),
+    .width = width, 
+    .height = height,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-    if(ctl->w != 0 && ctl->h != 0){
-    int W = inw(VGACTL_ADDR+2);  
-    int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-    uint32_t* p = ctl->pixels;
-
-    uint32_t p_pos = 0;
-    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-    uint32_t cp_byte = sizeof(uint32_t) * (w < W-x ? w : W - x);
-    for(int row = y; row < y + h; ++ row){
-        memcpy(fb + x + row * W, p + (p_pos), cp_byte);
-        // printf("write pixels[%d] of %d to fb[%d, %d]\n", p_pos, p[p_pos], row , x);
-        p_pos += w;
-      }
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (w == 0 || h == 0) return;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pi = ctl->pixels;
+  int i, j;              
+  for (i = 0; i < h; i ++) {
+    for (j = 0; j < w; j ++) {
+      fb[(y + i) * W + x + j] = pi[i * w + j];
     }
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
   }
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
 }
-
