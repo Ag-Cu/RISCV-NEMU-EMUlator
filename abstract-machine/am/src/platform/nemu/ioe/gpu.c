11417a86a4c35da9be75f91c8edfc980eb7121dd
tracer-ics2022 <tracer@njuics.org>
2023-02-27 15:41:55 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:41:55 up  3:49,  0 users,  load average: 0.27, 0.13, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -40,7 +40,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
       fb[(y + i) * W + x + j] = pi[i * w + j];
     }
   }
-  outl(SYNC_ADDR, 1);
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
