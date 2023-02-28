1dff706f971535d972551c0cf2b438eaeb7bea36
tracer-ics2022 <tracer@njuics.org>
2023-02-28 21:14:38 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:14:38 up  2:49,  0 users,  load average: 0.02, 0.02, 0.00

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -19,6 +19,7 @@ Context* __am_irq_handle(Context *c) {
           ev.event = EVENT_SYSCALL;
         }
         c->mepc += 4;     // skip the instruction that caused the trap
+        printf("PC += 4");
         break;
       default: ev.event = EVENT_ERROR; break;
     }
