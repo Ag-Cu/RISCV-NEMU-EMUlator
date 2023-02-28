454e4d2ce35004a66620c2a940127114c35a39b9
tracer-ics2022 <tracer@njuics.org>
2023-02-28 21:54:58 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:54:58 up  3:29,  0 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -50,6 +50,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   // exception trace
   #ifdef CONFIG_ETRACE_COND
     printf("------> Exception: %s\n", isa_intr_name(NO));
+    printf("------> mepc = %lx\n", CSRs.mepc);
   #endif
 
   return CSRs.mtvec;
