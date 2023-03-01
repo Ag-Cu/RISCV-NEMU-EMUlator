1ae1ef04504a132db200298624834b0e20e9edbe
tracer-ics2022 <tracer@njuics.org>
2023-03-01 19:49:42 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:49:42 up  2:38,  0 users,  load average: 0.16, 0.04, 0.01

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -73,6 +73,9 @@ void *_sbrk(intptr_t increment) {
     heap_end = (intptr_t)&_end;
     _syscall_(SYS_brk, heap_end, 0, 0);
   }
+  if (increment == 0) {
+    return (void *)heap_end;
+  }
   intptr_t old_heap_end = heap_end;
   intptr_t new_heap_end = heap_end + increment;
   intptr_t ret = _syscall_(SYS_brk, new_heap_end, 0, 0);
