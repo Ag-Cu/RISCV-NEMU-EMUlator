1f2c0d8fe4ca0f5054ce0dfddfdac47573a47558
tracer-ics2022 <tracer@njuics.org>
2023-03-01 19:10:22 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:10:22 up  1:59,  0 users,  load average: 0.09, 0.04, 0.01

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -63,6 +63,7 @@ size_t fs_write(int fd, void *buf, size_t len) {
   if (buf == NULL) { return 0; }
   for (size_t i = 0; i < len; i ++) {
     if (((char *)buf)[i] == 0) { return i; }
+    Log("output: %c", ((char *)buf)[i]);
     putch(((char *)buf)[i]);
   }
   return len;
