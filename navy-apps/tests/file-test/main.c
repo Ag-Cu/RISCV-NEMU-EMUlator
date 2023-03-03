e2d86f48d846c225019973f9cfc4c289108db0b5
tracer-ics2022 <tracer@njuics.org>
2023-03-03 20:18:37 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:18:37 up  3:05,  0 users,  load average: 0.03, 0.04, 0.01

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,16 +2,13 @@
 #include <assert.h>
 
 int main() {
+  printf("PASS!!!\n");
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
-
-  printf("open file success\n");
-
+  
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-
-  printf("first seek success\n");
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
@@ -20,21 +17,15 @@ int main() {
     assert(n == i + 1);
   }
 
-  printf("second seek success\n");
-
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
-  printf("third seek success\n");
-
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
-  printf("fourth seek success\n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
@@ -42,11 +33,7 @@ int main() {
     assert(n == i + 1 + 1000);
   }
 
-  printf("fifth seek success\n");
-
   fclose(fp);
-
-  printf("close file success\n");
 
   printf("PASS!!!\n");
 
