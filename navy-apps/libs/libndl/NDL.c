cbbcbfc57ef8833b046375c40dbeadad593bb671
tracer-ics2022 <tracer@njuics.org>
2023-03-05 00:21:22 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:21:22 up  7:56,  0 users,  load average: 0.01, 0.07, 0.06

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -66,9 +66,11 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   assert(w > 0 && w <= disp_size.w);
   assert(h > 0 && h <= disp_size.h);
 
-  // 重新设置x，y的值以居中显示
+  // w, h 为画布大小， 重新设置x，y的值以居中显示画布
   x = (disp_size.w - w) / 2;
   y = (disp_size.h - h) / 2;
+  
+
   for (size_t row = 0; row < h; ++row)
   {
     lseek(fbdev, x + (y + row) * disp_size.w, SEEK_SET);
