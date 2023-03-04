892abd81993f5b2a28569f85289fe1e345472134
tracer-ics2022 <tracer@njuics.org>
2023-03-05 00:10:55 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:10:55 up  7:45,  0 users,  load average: 0.04, 0.08, 0.02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -95,6 +95,9 @@ size_t fs_write(int fd, const void *buf, size_t len) {
     if (fd == FD_STDOUT || fd == FD_STDERR) {
         return f->write(buf, 0, len);
     }
+    if (fd == FD_VGA) {
+        return f->write(buf, f->open_offset, len);
+    }
 
     if (len == 0) return 0;
     if (f->open_offset >= f->size) return 0;
