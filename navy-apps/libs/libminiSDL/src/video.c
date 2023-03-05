4a41f7783f7bcf921cc5e7fdb506f187f9f79b57
tracer-ics2022 <tracer@njuics.org>
2023-03-05 15:16:03 +0800

    >  compile NEMU SA22225455 尹童 Linux LAPTOP-S449UV27 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:16:03 up  2:09,  0 users,  load average: 0.26, 0.23, 0.13

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -15,7 +15,8 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   assert(s);
   if (s->flags & SDL_HWSURFACE) {
-    NDL_DrawRect(s->pixels, x, y, w, h);
+    printf("Drawing rect at %d, %d, %d, %d", x, y, w, h);
+    NDL_DrawRect((uint32_t *)s->pixels, x, y, w, h);
   }
 }
 
